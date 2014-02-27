571837ed7d5d84d7c29dd235b32493039bdf6454
beckhamhamham <beckhamhamham@gmail.com>
2014-02-27 15:09:06 +0900

    add favicon, change show timing

--- main.js
+++ main.js
@@ -9,20 +9,30 @@ window.onload = function(){
 		}
 
 		function monster_show (monster) {
-			var line = $("<tr/>");
+			var line = $("<tr class=\"rare" + monster.rare + "\"/>");
 			line.append($("<td>" + monster.id + "</td>"));
 			line.append($("<td>" + monster.name + "</td>"));
 			line.append($("<td><a href=\"" + monster.url + "\"><img src=\"" + monster.img + "\"></a></td>"));
-			line.append($("<td>" + "未実装" + "</td>"));
+			line.append($("<td>" + monster.rare + "</td>"));
 			$("#monster_table").append(line);
 
-			console.log("lot = " + lot);
 		}
 
 		function lot(){
+			while(true){
+					var lot = (Math.floor(Math.random()*1250));
+					if(lot < monsters.length){
+						break;
+					}
+				}
+				return lot;
+		}
+
+		function show(){
 			var i=0;
 			do {
-				var lot = (Math.floor(Math.random()*25));
+					
+				
 				/*
 				if(lot < 3) {
 					var egg_result = "img/gomi.png";				
@@ -34,22 +44,36 @@ window.onload = function(){
 
 				$("#monster_img").attr("src",egg_result);
 				*/
-				monster_show(monsters[lot]);
+
+				setTimeout(function(){
+					var result = lot();
+					monster_show(monsters[result]);
+					console.log("lot=" + result + ", monster.id=" + monsters[result].id);
+				},  i*800);
 				i++;
 			} while (i<5);
 		}
 
+		console.log("start.");
+
+		//setTimeout(function() {
+		setup();
+		show();
+		//}, 10);
+/*
+		var limit = 20;
 		var execute = function(i) {
-			if(i<20){
+			if(i<limit){
 				setTimeout(function() {
 					setup();
-					lot();
 					i++;
+					lot(i == limit);
 					execute(i);
 				}, 10);
 			}
 		}
 
 		execute(0);
+		*/
 	});
 }
